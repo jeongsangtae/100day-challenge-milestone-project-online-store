8ad2839e76e7fdb3d27e4a1f13af2254a4d78bdc
jeongsangtae <wjdtkdxo55@naver.com>
2023-02-25 05:59:18 +0900

    Milestone Project Online Store - Add user input validation

---
+++ util/validation.js
@@ -0,0 +1,36 @@
+function isEmpty(value) {
+  return !value || value.trim() === "";
+}
+
+function userCredentialAreValid(email, password) {
+  return (
+    email && email.includes("@") && password && password.trim().length >= 6
+  );
+}
+
+function userDetailsAreValid(email, password, name, street, postal, city) {
+  return (
+    userCredentialAreValid(email, password) &&
+    !isEmpty(name) &&
+    !isEmpty(street) &&
+    !isEmpty(postal) &&
+    !isEmpty(city)
+    // name &&
+    // name.trim() !== ""
+    // street &&
+    // street.trim() !== ""
+    // postal &&
+    // postal.trim() !== ""
+    // city &&
+    // city.trim() !== ""
+  );
+}
+
+function emailIsConfirmed(email, confirmEmail) {
+  return email === confirmEmail;
+}
+
+module.exports = {
+  userDetailsAreValid: userDetailsAreValid,
+  emailIsConfirmed: emailIsConfirmed,
+};
