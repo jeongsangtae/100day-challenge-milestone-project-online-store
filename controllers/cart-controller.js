346c1dab2760f20574d54599b9438d19479c2bee
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-01 06:29:31 +0900

    Milestone Project Online Store - Cart logic

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -1,5 +1,22 @@
-function addCartItem(req, res) {
-  res.locals.cart.addItem();
+const Product = require("../models/product-model");
+
+async function addCartItem(req, res, next) {
+  let product;
+  try {
+    product = await Product.findById(req.body.productId);
+  } catch (error) {
+    next(error);
+    return;
+  }
+  const cart = res.locals.cart;
+
+  cart.addItem(product);
+  req.session.cart = cart;
+
+  res.status(201).json({
+    message: "장바구니 업데이트",
+    newTotalItems: cart.totalQuantity,
+  });
 }
 
 module.exports = {
