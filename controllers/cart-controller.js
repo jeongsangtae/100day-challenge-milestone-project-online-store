8f78741fe1b0cccbb34d502578f45aa18c7c6096
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-02 04:03:30 +0900

    Milestone Project Online Store - Update cart items (quantity change)

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -23,7 +23,28 @@ async function addCartItem(req, res, next) {
   });
 }
 
+function updateCartItem(req, res) {
+  const cart = res.locals.cart;
+
+  const updatedItemData = cart.updateItem(
+    req.body.productId,
+    req.body.quantity
+  );
+
+  req.session.cart = cart;
+
+  res.json({
+    message: "항목 업데이트!",
+    updatedCartData: {
+      newTotalQuantity: cart.totalQuantity,
+      newTotalPrice: cart.totalPrice,
+      updatedItemPrice: updatedItemData.updatedItemPrice,
+    },
+  });
+}
+
 module.exports = {
   addCartItem: addCartItem,
   getCart: getCart,
+  updateCartItem: updateCartItem,
 };
